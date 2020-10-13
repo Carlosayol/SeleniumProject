757666b95bb0aab07c27c0565264922ae2339bf1
Carlos Ayala​ <carlosayol@unisabana.edu.co>
2020-10-13 10:16:48 -0500

    Wrapper mejorado

--- utilities/handyWrappers.py
+++ utilities/handyWrappers.py
@@ -5,12 +5,21 @@ class HandyWrappers():
     def __init__(self, driver):
         self.driver = driver
 
+
     def getByType(self,locatorType):
         locatorType = locatorType.lower()
         if locatorType == "id":
             return By.ID
         elif locatorType == "xpath":
             return By.XPATH
+        elif locatorType == "css":
+            return By.CSS_SELECTOR
+        elif locatorType == "classname":
+            return By.CLASS_NAME
+        elif locatorType == "linktext":
+            return By.LINK_TEXT
+        elif locatorType == "name":
+            return By.NAME
         else:
             print("Locator type is not supported")
         return False
@@ -25,3 +34,25 @@ class HandyWrappers():
         except:
             print("Element not found")
         return element
+
+    def isElementPresent(self, locator, byType):
+        try:
+            element = self.driver.find_element(byType, locator)
+            if element is not None:
+                print("Element Found")
+                return True
+            else: return False
+        except:
+            print("Element not found")
+            return False
+
+    def ElementsPresent(self, locator, byType):
+        try:
+            elements = self.driver.find_elements(byType, locator)
+            if len(elements)>0:
+                print("Existen Elementos")
+                return True
+            else: return False
+        except:
+            print("No hay Elementos")
+            return False
