a1eb88ea94e92323a700b5aa5637e5d181b2aab8
Carlos Ayala​ <carlosayol@unisabana.edu.co>
2020-10-19 13:59:13 -0500

    practicando scripts

---
+++ advanced/windowSize.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class WindowSize():
+
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com/p/practice"
+        driver = webdriver.Chrome(executable_path="../basic/driver/chromedriver.exe")
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(3)
+
+        height = driver.execute_script("return window.innerHeight;")
+        width = driver.execute_script("return window.innerWidth;")
+
+        print("Ancho de la pagina ",width)
+        print("Alto de la pagina ",height)
+
+
+fn = WindowSize()
+fn.test()
