ce8e71bd3fab2603d8a035f0b5f401b800f6af56
Carlos Ayala​ <carlosayol@unisabana.edu.co>
2020-11-05 09:29:59 -0500

    agregando argumentos al conftest

--- pytestpackage/conftest.py
+++ pytestpackage/conftest.py
@@ -9,7 +9,23 @@ def setUp():
 
 #Module level
 @pytest.fixture(scope="module")
-def ModulesetUp():
+def ModulesetUp(browser, osType):
     print("Running module level setup")
+    if browser=="chrome":
+        print("Running test on Chrome")
+    else:
+        print("Running test on FF")
     yield
     print("Running module level tear down")
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--osType",help="Type of operating system")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
---
+++ pytestpackage/test_command_line_args.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_command_methodA(ModulesetUp,setUp):
+    print("Running command method A")
+
+def test_command_methodB(ModulesetUp,setUp):
+    print("Running command method B")
