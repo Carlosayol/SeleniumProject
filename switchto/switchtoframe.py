e74c28e2609fcc12d37f127a541abc9f0164d866
carlosayol <Carlosaolar@gmail.com>
2021-01-07 12:28:50 -0500

    small method to verify in which iframe is x element

--- switchto/switchtoframe.py
+++ switchto/switchtoframe.py
@@ -19,27 +19,48 @@ class switchToFrame():
         #driver.switch_to.frame("courses-iframe")
 
         #Usando posicion en arreglo"
-        #driver.switch_to.frame(1)
+        driver.switch_to.frame(0)
 
-        #credit_input = driver.find_element(By.NAME, "cardnumber")
-        #credit_input.send_keys("1111111111111111")
+        credit_input = driver.find_element(By.NAME, "cardnumber")
+        credit_input.send_keys("1111111111111111")
 
-        #driver.switch_to.default_content()
+        driver.switch_to.default_content()
 
-        #driver.switch_to.frame(4)
+        # iframe_list = driver.find_elements(By.XPATH,"//iframe")
+        # print(len(iframe_list))
+        # for  i in range (len(iframe_list)):
+        #     driver.switch_to.frame(iframe_list[i])
+        #     try:
+        #         element = driver.find_element(By.NAME, "cardnumber")
+        #         if element is not None:
+        #             print("element found at index ", i)
+        #     except:
+        #         print("f")
+        #     driver.switch_to.default_content()
 
-        #credit_input2 = driver.find_element(By.NAME, "exp-date")
-        #credit_input2.send_keys(1234)
+        driver.switch_to.frame(1)
 
-        enroll_buttons = driver.find_elements(By.XPATH,"//div[@class='stripe-outer ']//button")
-        enroll_button = enroll_buttons[2]
-        enroll_button.click()
+        credit_input2 = driver.find_element(By.NAME, "exp-date")
+        credit_input2.send_keys(1234)
+
+        driver.switch_to.default_content()
+
+
+        #enroll_buttons = driver.find_elements(By.XPATH,"//div[@class='stripe-outer ']//button")
+        #enroll_button = enroll_buttons[2]
+        #enroll_button.click()
         #print(enroll_buttons)
 
         time.sleep(2)
 
-        #driver.switch_to.default_content()
+
         #driver.execute_script("window.scrollBy(0,-1000)")
+
+
+
+
+
+
         driver.quit()
 
 
